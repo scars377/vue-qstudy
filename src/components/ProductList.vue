c4a39978312583df171199aab25915e2a0e10756
Scars Yao <scars@17.media>
2018-04-28 14:51:28 +0800

    fetch

---
+++ src/components/ProductList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      products: [],
+    };
+  },
+  mounted() {
+    fetch('https://webcdn.17app.co/campaign/1804-tw-full-of-you-5-s2.json');
+  },
+};
+</script>
