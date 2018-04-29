4564f3eb092c05c355bee949cb8b9b3f05f50be8
Scars Yao <scars@17.media>
2018-04-29 16:31:25 +0800

    localStorage

--- src/store.js
+++ src/store.js
@@ -3,16 +3,25 @@ import Vue from 'vue';
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {
+let initState;
+try {
+  initState = JSON.parse(localStorage.state);
+} catch (err) {
+  initState = {
     cart: [],
-  },
+  };
+}
+
+export default new Vuex.Store({
+  state: initState,
+
   getters: {
 
   },
   mutations: {
     addToCart(state, item) {
       state.cart = state.cart.concat(item);
+      localStorage.state = JSON.stringify(state);
     },
   },
   actions: {
